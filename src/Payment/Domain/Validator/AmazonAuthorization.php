e96ac2ab862d2324ec11ec869ac5f61f591318e8
MicheleBeargroup <michele@beargroup.com>
2018-03-28 19:05:56 -0600

    Update re: AP-217

--- src/Payment/Domain/Validator/AmazonAuthorization.php
+++ src/Payment/Domain/Validator/AmazonAuthorization.php
@@ -13,61 +13,63 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 namespace Amazon\Payment\Domain\Validator;
 
 use Amazon\Payment\Domain\AmazonAuthorizationStatus;
 use Amazon\Payment\Domain\Details\AmazonAuthorizationDetails;
-use Amazon\Payment\Exception\HardDeclineException;
-use Amazon\Payment\Exception\SoftDeclineException;
-use Amazon\Payment\Exception\TransactionTimeoutException;
-use Magento\Framework\Exception\StateException;
 
+/**
+ * Class AmazonAuthorization
+ * @package Amazon\Payment\Domain\Validator
+ */
 class AmazonAuthorization
 {
     /**
      * @param AmazonAuthorizationDetails $details
-     *
-     * @return bool
-     * @throws HardDeclineException
-     * @throws SoftDeclineException
-     * @throws StateException
+     * @return array
      */
     public function validate(AmazonAuthorizationDetails $details)
     {
         $status = $details->getStatus();
 
         switch ($status->getState()) {
-        case AmazonAuthorizationStatus::STATE_CLOSED:
-            switch ($status->getReasonCode()) {
-            case AmazonAuthorizationStatus::REASON_MAX_CAPTURES_PROCESSED:
-                return true;
-            }
-            break;
-        case AmazonAuthorizationStatus::STATE_OPEN:
-        case AmazonAuthorizationStatus::STATE_PENDING:
-            return true;
-        case AmazonAuthorizationStatus::STATE_DECLINED:
-            $this->throwDeclinedExceptionForStatus($status);
+            case AmazonAuthorizationStatus::STATE_CLOSED:
+                switch ($status->getReasonCode()) {
+                    case AmazonAuthorizationStatus::REASON_MAX_CAPTURES_PROCESSED:
+                        return [
+                            'result' => true,
+                            'reason' => AmazonAuthorizationStatus::REASON_MAX_CAPTURES_PROCESSED
+                        ];
+                }
+                break;
+            case AmazonAuthorizationStatus::STATE_OPEN:
+            case AmazonAuthorizationStatus::STATE_PENDING:
+                return ['result' => true, 'reason' => $status->getState()];
+            case AmazonAuthorizationStatus::STATE_DECLINED:
+                return ['result' => false, 'reason' => $this->getReasonCode($status)];
         }
 
-        throw new StateException($this->getExceptionMessage($status));
+        return ['result' => false, 'reason' => $status->getState()];
     }
 
-    protected function throwDeclinedExceptionForStatus(AmazonAuthorizationStatus $status)
+    /**
+     * Need to ensure three specific reason codes come through during processing.
+     *
+     * @param AmazonAuthorizationStatus $status
+     * @return null|string
+     */
+    protected function getReasonCode(AmazonAuthorizationStatus $status)
     {
         switch ($status->getReasonCode()) {
-        case AmazonAuthorizationStatus::REASON_TRANSACTION_TIMEOUT:
-            throw new TransactionTimeoutException($this->getExceptionMessage($status));
-        case AmazonAuthorizationStatus::REASON_AMAZON_REJECTED:
-        case AmazonAuthorizationStatus::REASON_PROCESSING_FAILURE:
-            throw new HardDeclineException($this->getExceptionMessage($status));
-        case AmazonAuthorizationStatus::REASON_INVALID_PAYMENT_METHOD:
-            throw new SoftDeclineException($this->getExceptionMessage($status));
+            case AmazonAuthorizationStatus::REASON_TRANSACTION_TIMEOUT:
+                return 'timeout';
+            case AmazonAuthorizationStatus::REASON_AMAZON_REJECTED:
+            case AmazonAuthorizationStatus::REASON_PROCESSING_FAILURE:
+                return 'hard_decline';
+            case AmazonAuthorizationStatus::REASON_INVALID_PAYMENT_METHOD:
+                return 'soft_decline';
         }
-    }
-
-    protected function getExceptionMessage(AmazonAuthorizationStatus $status)
-    {
-        return __('Amazon authorize invalid state : %1 with reason %2', $status->getState(), $status->getReasonCode());
+        return $status->getReasonCode();
     }
 }
