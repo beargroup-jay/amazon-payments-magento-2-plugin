4545912b470e16269223cc801c36f975e9b255cf
MicheleBeargroup <michele@beargroup.com>
2018-04-26 14:09:59 -0600

    Update re: AP-341

--- src/Core/Setup/UpgradeSchema.php
+++ src/Core/Setup/UpgradeSchema.php
@@ -13,7 +13,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-namespace Amazon\Login\Setup;
+namespace Amazon\Core\Setup;
 
 use Amazon\Login\Model\ResourceModel\CustomerLink;
 use Magento\Framework\DB\Adapter\AdapterInterface;
